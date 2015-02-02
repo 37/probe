f97258457ce5fd88347477de4c88404af61ced70
root <root@kivypie.(none)>
2015-02-02 04:15:37 +0000

    Comment: Great progress on base message slide animations. To be build: Cloud + file syncing.

--- prometheus.py
+++ prometheus.py
@@ -4,23 +4,32 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.animation import Animation
-from kivy.core.window import Window
 
-class MainScreen (Widget):
+class MainScreen (BoxLayout):
+    active_label = ObjectProperty(None)
     f = open("sourcefile.txt", 'r')
     lines = f.readlines()
     f.close
     i = 0	
 
-    def ChangeBtn(self, **kwargs):
+    def Animate(self):
+        right = Animation(x=self.active_label.parent.width, color= [0,0,0,0])
+        fade = Animation(color= [0,0,0,0], duration=0.5)
+        fade.bind(on_complete=self.ChangeBtn) 
+        snap = Animation(x=0, duration=0.01)
+        wait = Animation(duration = 5)
+        left = Animation(center_x=self.active_label.parent.width/2, color= [1,1,1,1])
+        anim = fade + snap + left + wait + right
+        anim.start(self.active_label)
+
+    def ChangeBtn(self, instance, value):
     	current_label = self.lines[self.i]
     	label = self.ids['label_display']
-    	label.text = current_label
+        label.text = current_label
         self.i += 1
 
 class PrometheusApp(App):
